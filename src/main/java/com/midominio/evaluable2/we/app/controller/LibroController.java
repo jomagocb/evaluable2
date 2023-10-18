9c2be6affc4500e3b91c5811395d7bfee08dcbbb
jomagocb <jomagocb@gmail.com>
2023-10-18 21:21:45 +0200

    intentando hacer el boton de añadir

--- src/main/java/com/midominio/evaluable2/we/app/controller/LibroController.java
+++ src/main/java/com/midominio/evaluable2/we/app/controller/LibroController.java
@@ -1,29 +1,52 @@
 package com.midominio.evaluable2.we.app.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.midominio.evaluable2.we.app.model.dao.LibroRepository;
+import com.midominio.evaluable2.we.app.model.entity.Libro;
+import com.midominio.evaluable2.we.app.service.LibroService;
 
 @Controller
 @RequestMapping("/catalogo")
 public class LibroController {
 
 	@Autowired
-	LibroRepository libroRepository;
+	LibroService libroService;
 	
 	@ModelAttribute("tituloNavegador")
 	private String tituloNavegador() {return "Biblioteca";}
 	
 	@GetMapping("/completo")
 	public String listaCompleta(Model model) {
-		model.addAttribute("listadoCompleto", libroRepository.findAll());
-		
+		model.addAttribute("listadoCompleto", libroService.findAll());
 		return "/libro/catalogo-completo";
 	}
+	@GetMapping("/completo/un-libro-mas/{id}")
+	public String sumaUnStock(Model model,@PathVariable Long id) {
+		libroService.sumaUnLibro(id);
+		model.addAttribute("listadoCompleto", libroService.findAll());
+
+		return "/libro/catalogo-completo";
+	}
+	
+	/*@GetMapping("/info")
+	public String infoLibro(@RequestParam(required = false) Long id,Model model) {
+		model.addAttribute("titulo", "Información detallada del libro");
+	
+		
+		titulo
+		nombre del libro
+		información detallada
+		botones alta baja
+		
+		
+		return "/libro/info-libro";*/
 	
 }
--- src/main/java/com/midominio/evaluable2/we/app/service/LibroService.java
+++ src/main/java/com/midominio/evaluable2/we/app/service/LibroService.java
@@ -1,9 +1,32 @@
 package com.midominio.evaluable2.we.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.midominio.evaluable2.we.app.model.dao.LibroRepository;
+import com.midominio.evaluable2.we.app.model.entity.Libro;
 
+@Service
 public class LibroService {
 
+	@Autowired
+	public LibroRepository libroRepository;
+	
+	public Iterable<Libro> findAll(){
+		return libroRepository.findAll();
+	}
+	public int sumaUnLibro(Long id) {
+		Libro libro = libroRepository.findById(id).orElse(null);
+		int cantidadLibros = libro.getCantidadLibros();
+		cantidadLibros++;
+		libro.setCantidadLibros(cantidadLibros);
+		libroRepository.save(libro);
+		return cantidadLibros; //revisa el return, ¿Que quiero que me muestre?
+		//get por id, sumo uno y save.
+		
+	}
+	
+	
 	
 	
 }
